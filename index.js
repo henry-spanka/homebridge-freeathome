1298ebd7c40bcc846b0fba59883db03a6eb3cc0d
Henry Spanka <henry@henryspanka.com>
2018-03-08 11:33:11 +0100

    remove unneeded variables

--- index.js
+++ index.js
@@ -2,20 +2,16 @@
 
 const path = require('path');
 
-var Accessory, Service, Characteristic, UUIDGen;
+var Service, Characteristic
 
 const WebSocket = require('ws');
 
 module.exports = function(homebridge) {
     console.log("homebridge API version: " + homebridge.version);
 
-    // Accessory must be created from PlatformAccessory Constructor
-    Accessory = homebridge.platformAccessory;
-
     // Service and Characteristic are from hap-nodejs
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
-    UUIDGen = homebridge.hap.uuid;
 
     // For platform plugin to be considered as dynamic platform plugin,
     // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
